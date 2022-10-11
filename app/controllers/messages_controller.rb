bf1d42334686531679598763d303511bade114b7
Aitzaz94 <aitzazakmal@gmail.com>
2022-10-11 12:52:27 +0500

    added images to users, in profile&messaging

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+
     def create
         @message = current_user.messages.create(body: msg_params[:body], room_id: params[:room_id])
     end
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module Turbochat
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    # config.active_storage.variant_processor = :image_magick
   end
 end
